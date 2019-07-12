4418a016e7c3d5c7316be40f7865db2072518b42
tomaszlach <tomaszlach86@gmail.com>
2019-07-12 08:29:36 +0200

    complete solution

--- src/TextProcessingServlet.java
+++ src/TextProcessingServlet.java
@@ -1,15 +1,28 @@
+import logic.TextProcessor;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "TextProcessingServlet")
+@WebServlet("/process")
 public class TextProcessingServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String text = request.getParameter("text");
 
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+
+        writer.println(text);
+        writer.println("Ilość słów: "+TextProcessor.getCharAmount(text));
+        writer.println("Ilość znaków: "+TextProcessor.getCharAmountWithoutSpace(text));
+        writer.println("Ilość znaków (bez spacji): "+TextProcessor.getWordCount(text));
+        writer.println("Palindrom: "+TextProcessor.isPalindrome(text));
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
